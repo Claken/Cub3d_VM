a53b817813c7736766921fa7dc926735a1dce9e4
Claken <samychouam@gmail.com>
2021-03-30 23:34:43 +0200

    cub3d 30 03 2021

--- cub3d_srcs/images_handling/images_management.c
+++ cub3d_srcs/images_handling/images_management.c
@@ -6,36 +6,37 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 13:48:43 by sachouam          #+#    #+#             */
-/*   Updated: 2021/03/28 01:08:07 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/03/30 23:30:48 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cub3d_includes/cub3d.h"
 
-// parcourir la longueur et la largeur de la texture ?
-// if size = n, appeler fonction avec texture n ?
-// utiliser pixend ? pixbeg ? colhei ?
-
 static void
-	ft_test_text_map(t_all *all, int x, t_images *texture)
+	ft_texture_mapping(t_all *all, int x, t_images *texture)
 {
-	int y;
-	double i;
-	double j;
-	double test;
+	int	y;
+	double	i;
+	double	texty;
+	double	textx;
 
-	y = all->disp.pixbeg - 1;
-	i = 0;
+	if (all->disp.pixbeg < 0)
+		y = 0;
+	else
+		y = all->disp.pixbeg;
+	i = (double)texture->height / (double)all->disp.colhei;
+	texty = (y - (all->data.reshei / 2 - all->disp.colhei / 2)) * i;
 	if (all->vect.side == 2 || all->vect.side == 4)
-		j = texture->width * fmod(all->vect.fvy, CASE);
+		textx = texture->width * fmod(all->vect.fvy, CASE);
 	else if (all->vect.side == 1 || all->vect.side == 3)
-		j = texture->width * fmod(all->vect.fhx, CASE);
-	test = 1 / ((double)all->disp.colhei / (double)texture->height);
-	while (++y <= all->disp.pixend)
+		textx = texture->width * fmod(all->vect.fhx, CASE);
+	while (y <= all->disp.pixend)
 	{
-		all->disp.addr[y * all->data.reswid + x]
-		= texture->addr[(int)i * texture->width + (int)j];
-		i += test;
+		all->disp.color = texture->addr[(int)texty
+		* texture->width + (int)textx];
+		all->disp.addr[y * all->data.reswid + x] = all->disp.color;
+		texty += i;
+		y++;
 	}
 }
 
@@ -43,13 +44,13 @@ static void
 	ft_text_according_to_side(t_all *all, int index)
 {
 	if (all->vect.side == 1)
-		ft_test_text_map(all, index, &all->image.north);
+		ft_texture_mapping(all, index, &all->image.north);
 	else if (all->vect.side == 2)
-		ft_test_text_map(all, index, &all->image.east);
+		ft_texture_mapping(all, index, &all->image.east);
 	else if (all->vect.side == 3)
-		ft_test_text_map(all, index, &all->image.south);
+		ft_texture_mapping(all, index, &all->image.south);
 	else if (all->vect.side == 4)
-		ft_test_text_map(all, index, &all->image.west);
+		ft_texture_mapping(all, index, &all->image.west);
 }
 
 void
